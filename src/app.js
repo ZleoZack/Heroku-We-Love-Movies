ab8bf8d0946393231ade94fff1710bfe5472caf5
Zackary Campbell <zleonlion@gmail.com>
2022-09-28 01:07:09 -0500

    Second Commit

--- src/app.js
+++ src/app.js
@@ -8,27 +8,20 @@ const theatersRouter = require("./theaters/theaters.router");
 
 const cors = require("cors");
 
-
 const app = express();
 
 //routes
 
 app.use(cors());
+
 app.use(express.json());
 
 app.use("/movies", moviesRouter);
 app.use("/reviews", reviewsRouter);
 app.use("/theaters", theatersRouter);
 
-
-
-
 //routes for error handling/ 404 not found requests, app.use(highest priority when being invoked in the app file, these routes will always be checked through)
 app.use(notFound);
 app.use(errorHandler);
-
-
-
-
 
 module.exports = app;
